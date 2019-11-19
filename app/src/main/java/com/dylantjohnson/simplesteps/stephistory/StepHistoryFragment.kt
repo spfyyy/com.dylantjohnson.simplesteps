bda9a545e3b775887dc668520fc67071a9d42b31
Dylan Johnson <spfyyy@gmail.com>
2019-11-18 22:02:03 -0800

    Enabled data binding and converted StepHistoryFragment to use it.

--- app/src/main/java/com/dylantjohnson/simplesteps/stephistory/StepHistoryFragment.kt
+++ app/src/main/java/com/dylantjohnson/simplesteps/stephistory/StepHistoryFragment.kt
@@ -1,17 +1,20 @@
 package com.dylantjohnson.simplesteps.stephistory
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-import com.dylantjohnson.simplesteps.R
+import com.dylantjohnson.simplesteps.databinding.FragmentStepHistoryBinding
 
 class StepHistoryFragment : Fragment() {
+    private lateinit var mBinding: FragmentStepHistoryBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_step_history, container, false)
+        mBinding = FragmentStepHistoryBinding.inflate(inflater, container, false)
+        mBinding.lifecycleOwner = this
+        return mBinding.root
     }
 }
