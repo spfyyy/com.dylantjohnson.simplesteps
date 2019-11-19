2e9185e273c0839fe43be1db29e8416f6674266e
Dylan Johnson <spfyyy@gmail.com>
2019-11-18 17:51:15 -0800

    Initial Commit. Created fresh Android project.

---
+++ app/src/main/java/com/dylantjohnson/simplesteps/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dylantjohnson.simplesteps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
